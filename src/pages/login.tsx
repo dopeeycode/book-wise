5b8dc39adacb2108f6a134d61d90c63b9a5e1c71
Italo <italodopeey@gmail.com>
2023-09-04 09:43:49 -0300

    feat: page login

---
+++ src/pages/login.tsx
@@ -0,0 +1,29 @@
+import AuthButtons from '@/components/AuthButtons'
+import { Heading, Text } from '@/components/Typography'
+import {
+  LoginContainer,
+  LogoSection,
+  WelcomeSection,
+} from '@/styles/pages/login'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <LoginContainer>
+      <LogoSection>
+        <img src="/images/logo.svg" alt="BookWise Logo" />
+      </LogoSection>
+
+      <WelcomeSection>
+        <Heading size="lg" color="gray-100">
+          Boas vindas !
+        </Heading>
+        <Text color="gray-200">Faça seu login ou acesse como visitante.</Text>
+
+        <AuthButtons />
+      </WelcomeSection>
+    </LoginContainer>
+  )
+}
+
+export default Login
